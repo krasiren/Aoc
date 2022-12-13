97701f1f1141220799798cba8201baba15d66caa
krasiren <64616509+krasiren@users.noreply.github.com>
2022-12-13 14:38:03 +0100

    day13

---
+++ 2022/days 10-19/day13/day13.py
@@ -0,0 +1,72 @@
+from functools import cmp_to_key
+
+
+def order(a, b) -> int:
+    """Recursion, that compares"""
+    if type(a) == type(b) == int:
+        if a < b:
+            return 1
+        elif a > b:
+            return -1
+        else:
+            return 0
+    elif type(a) == type(b) == list:
+        for i, j in zip(a, b):
+            res = order(i, j)
+            if res != 0:
+                return res
+        if len(a) < len(b):
+            return 1
+        elif len(a) > len(b):
+            return -1
+        else:
+            return 0
+    else:
+        if type(a) == int:
+            return order([a], b)
+        else:
+            return order(a, [b])
+
+
+def makeList(x: str) -> list[int]:
+    """Hehe, yeah boi"""
+    loc = {}
+    exec('x = ' + x, globals(), loc)
+    return loc['x']
+
+
+def first(input: str) -> None:
+    sum = 0
+    pairs = input.split('\n\n')
+    for i, pair in enumerate(pairs):
+        a, b = pair.split('\n')[0], pair.split('\n')[1]
+        a, b = makeList(a), makeList(b)
+
+        if order(a, b) == True:
+            sum += i+1
+
+    print(sum)
+
+
+def second(input: str, x, y) -> None:
+    ls = []
+    pairs = input.split('\n\n')
+    for pair in pairs:
+        a, b = pair.split('\n')[0], pair.split('\n')[1]
+        a, b = makeList(a), makeList(b)
+        ls.append(a)
+        ls.append(b)
+    ls.append(x)
+    ls.append(y)
+
+    ls = sorted(ls, key=cmp_to_key(order), reverse=True)
+    i, j = ls.index(x)+1, ls.index(y)+1
+    print(i * j)
+
+
+if __name__ == "__main__":
+    with open('in.txt') as f:
+        input = f.read()
+
+    first(input)
+    second(input, [[2]], [[6]])
