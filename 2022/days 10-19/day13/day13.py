0c268d93647350ffa53a95a14939f339c9821551
krasiren <64616509+krasiren@users.noreply.github.com>
2022-12-13 23:07:03 +0100

    Update day13.py
    makeList -> eval

--- 2022/days 10-19/day13/day13.py
+++ 2022/days 10-19/day13/day13.py
@@ -28,19 +28,12 @@ def order(a, b) -> int:
             return order(a, [b])
 
 
-def makeList(x: str) -> list[int]:
-    """Hehe, yeah boi"""
-    loc = {}
-    exec('x = ' + x, globals(), loc)
-    return loc['x']
-
-
 def first(input: str) -> None:
     sum = 0
     pairs = input.split('\n\n')
     for i, pair in enumerate(pairs):
         a, b = pair.split('\n')[0], pair.split('\n')[1]
-        a, b = makeList(a), makeList(b)
+        a, b = eval(a), eval(b)
 
         if order(a, b) == 1:
             sum += i+1
@@ -53,8 +46,8 @@ def second(input: str, x, y) -> None:
     pairs = input.split('\n\n')
     for pair in pairs:
         a, b = pair.split('\n')[0], pair.split('\n')[1]
-        ls.append(makeList(a))
-        ls.append(makeList(b))
+        ls.append(eval(a))
+        ls.append(eval(b))
 
     ls = sorted(ls, key=cmp_to_key(order), reverse=True)
     i, j = ls.index(x)+1, ls.index(y)+1
